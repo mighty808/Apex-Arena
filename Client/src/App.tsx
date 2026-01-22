679beeff5d55be7955e023c70fa014581350c825
mighty808 <effahaboagye888@gmail.com>
2026-01-22 23:53:47 +0000

    style changes on App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
 
 const Layout = () => (
-  <div className="min-h-dvh flex flex-col">
+  <div className="min-h-dvh flex flex-col bg-white text-gray-800">
     <Navbar />
     <main className="flex-1">
       <Outlet />
     </main>
+    <Footer />
   </div>
 );
-
 
 const App = () => {
   return (
