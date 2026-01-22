acb07e68a94a0639582fa451a277647fe5eea48c
mighty808 <effahaboagye888@gmail.com>
2026-01-22 00:25:29 +0000

    routes underway

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,27 @@
+import { Routes, Route, Outlet } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar";
+import SignUp from "./pages/SignUp";
+
+const Layout = () => (
+  <div className="min-h-dvh flex flex-col">
+    <Navbar />
+    <main className="flex-1">
+      <Outlet />
+    </main>
+  </div>
+);
+
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      App Working
-    </div>
-  )
-}
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Landing />} />
+        <Route path="signup" element={<SignUp />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
